ec2e554be31988fc0c169b82cfeac7ae87396a24
tasfi60 <tasfiagirl@gmail.com>
2022-10-27 02:40:23 +0600

    fake json added

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const courses = require('./data/courses.json');
+const coursedetails = require('./data/coursedetails.json');
+
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
   })
+
+app.get('/courses/:id', (req, res) => {
+   
+    const id = req.params.id;
+    const category_news = coursedetails.filter(n => n.category_id === id);
+    res.send(category_news);
+
+})
+    
+
+
 
 
   app.listen(port, () => {
